4cb4b7c54ffff869ea3ec35caa4e079cfa2aad34
AUMikkel <202007669@post.au.dk>
2023-12-19 09:41:22 +0100

    Removed raspberry pi

--- Week6/rest-server.py
+++ Week6/rest-server.py
@@ -274,4 +274,4 @@ def server_error(e):
 # Start the Flask app (must be after the endpoint functions) 
 host_local_computer = "localhost" # Listen for connections on the local computer
 host_local_network = "0.0.0.0" # Listen for connections on the local network
-app.run(host=host_local_network if is_raspberry_pi() else host_local_computer, port=9000)
+app.run(host= host_local_computer, port=9000)
