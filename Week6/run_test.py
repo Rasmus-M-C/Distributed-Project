b2057c90c3b376e4d112806d4eda21a84c42574e
AUMikkel <202007669@post.au.dk>
2023-12-19 15:40:25 +0100

    using python evn

--- Week6/run_test.py
+++ Week6/run_test.py
@@ -1,8 +1,9 @@
 import subprocess
 from time import sleep
 
+number_of_nodes = 3
 # List of Python files to run
-python_files = ["storage-node.py" for _ in range(9)] + [ "rest-server.py"]
+python_files = ["storage-node.py" for _ in range(number_of_nodes)] + [ "rest-server.py"]
 
 
 
@@ -12,13 +13,13 @@ processes = []
 
 i = 1
 for script_file in python_files:
-    if i <= 9:
+    if i <= number_of_nodes:
         print("Running ", script_file, " with args ", f"node0{i}", f"{i}")
-        process = subprocess.Popen(["python3", script_file] + [f"node{i}", f"{i}"])
+        process = subprocess.Popen(["python", script_file] + [f"node{i}", f"{i}"])
         i += 1
     else:
         print("Running ", script_file)
-        process = subprocess.Popen(["python3", script_file])
+        process = subprocess.Popen(["python", script_file])
 
     processes.append(process)
     sleep(0.2)
