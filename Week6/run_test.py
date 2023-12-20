d9522c826acf5ecdcec45b37823eda051115d31f
AUMikkel <202007669@post.au.dk>
2023-12-20 09:34:02 +0100

    Added terminate after set time

--- Week6/run_test.py
+++ Week6/run_test.py
@@ -1,48 +1,64 @@
 import subprocess
 from time import sleep
 import random
+import sys
 
-N = 26
+N = int(sys.argv[0]) # Number of nodes to run
 
 number_of_nodes = 3
 # List of Python files to run
 python_files = ["storage-node.py" for _ in range(N)] + [ "rest-server.py"]
 
 # Run each Python script with its corresponding arguments
-processes = []
-
+processes_storage_nodes = []
+processes_rest_server = None
 i = 1
 for script_file in python_files:
     if i <= N:
         print("Running ", script_file, " with args ", f"node0{i}", f"{i}")
-        process = subprocess.Popen(["python", script_file] + [f"node{i}", f"{i}"])
+        process = subprocess.Popen(
+            ["python", script_file] + [f"node{i}", f"{i}"],
+            shell=True
+        )
+        print("Process ID: ", process.pid)
+        processes_storage_nodes.append(process)
         i += 1
     else:
-        print("Running ", script_file, " with args ", f"{N}")
-        process = subprocess.Popen(["python3", script_file] + [f"{N}"])
-
-    processes.append(process)
+        print("Running ", script_file)
+        process = subprocess.Popen(
+            ["python", script_file],
+            shell=True
+        )
+        print("Process ID: ", process.pid)
+        processes_rest_server = process
     sleep(0.2)
     
 # Set the delay time and number of processes to terminate
 delay_time_seconds = 5  # Set the delay time in seconds
 num_processes_to_terminate = 2  # Set the number of processes to terminate randomly
 
+# Wait for the specified delay time
+sleep(delay_time_seconds)
+
 # Get the PIDs of the processes
-pids = [str(process.pid) for process in processes]
+pids = [str(process.pid) for process in processes_storage_nodes]
 
 # Start the subprocess to terminate processes after the set time
 #terminate_process = subprocess.Popen(["python", "terminate_processes.py", str(num_processes_to_terminate), str(delay_time_seconds)]+pids)
 
-processes_to_kill = random.sample(processes, num_processes_to_terminate)
+processes_to_kill = random.sample(processes_storage_nodes, num_processes_to_terminate)
+print("processes_to_kill: ", pids)
 
-    
-# Wait for all subprocesses to finish
-for i, process in enumerate(processes):
+# Terminating processes
+for i, process in enumerate(processes_storage_nodes):
     if process in processes_to_kill:
-        sleep(5)
         process.terminate()
-        sleep(0.2)
         print(f"Terminated process {pids[i]}")
-    else:
-        process.wait()
+
+# Wait for all subprocesses to finish
+for process in processes_storage_nodes:
+    process.wait()
+    print(f"Process {process.pid} finished")
+
+processes_rest_server.wait()
+print(f"Process {processes_rest_server.pid} finished")
