5915a0d2db37ad4dffe9292caee89cd09426120c
AUMikkel <202007669@post.au.dk>
2023-12-23 09:48:55 +0100

    something

--- Week6/env_run_test.py
+++ Week6/env_run_test.py
@@ -3,34 +3,34 @@ from time import sleep
 import random
 import sys
 
-N = int(sys.argv[1]) # Number of nodes to run
-# List of Python files to run
-python_files = ["storage-node.py" for _ in range(N)] + ["rest-server.py"]
+# N = int(sys.argv[1]) # Number of nodes to run
+# # List of Python files to run
+# python_files = ["storage-node.py" for _ in range(N)] + ["rest-server.py"]
 
-# Activate virtual environment
-activate_env = r"C:\Users\m-esp\Documents\DistributedStorage\env\Scripts\activate.bat "  # Replace with the path to your virtualenv activation script
+# # Activate virtual environment
+# activate_env = r"C:\Users\m-esp\Documents\DistributedStorage\env\Scripts\activate.bat "  # Replace with the path to your virtualenv activation script
 
-# Run each Python script with its corresponding arguments
-processes_storage_nodes = []
-processes_rest_server = None
-i = 1
-for script_file in python_files:
-    if i <= N:
-        print("Running ", script_file, " with args ", f"node0{i}", f"{i}")
-        process = subprocess.Popen(
-            [activate_env, "&&", "python", script_file] + [f"node{i}", f"{i}"]
-        )
-        print("Process ID: ", process.pid)
-        processes_storage_nodes.append(process)
-        i += 1
-    else:
-        print("Running ", script_file)
-        process = subprocess.Popen(
-            [activate_env, "&&", "python", script_file] + [f" {N}"]
-        )
-        print("Process ID: ", process.pid)
-        processes_rest_server = process
-    sleep(0.2)
+# # Run each Python script with its corresponding arguments
+# processes_storage_nodes = []
+# processes_rest_server = None
+# i = 1
+# for script_file in python_files:
+#     if i <= N:
+#         print("Running ", script_file, " with args ", f"node0{i}", f"{i}")
+#         process = subprocess.Popen(
+#             [activate_env, "&&", "python", script_file] + [f"node{i}", f"{i}"]
+#         )
+#         print("Process ID: ", process.pid)
+#         processes_storage_nodes.append(process)
+#         i += 1
+#     else:
+#         print("Running ", script_file)
+#         process = subprocess.Popen(
+#             [activate_env, "&&", "python", script_file] + [f" {N}"]
+#         )
+#         print("Process ID: ", process.pid)
+#         processes_rest_server = process
+#     sleep(0.2)
 #random_node = random.randint(0, N-1)
 #number_of_nodes_to_kill = 2
 #random_nodes = random.sample(processes_storage_nodes, k = number_of_nodes_to_kill)
@@ -64,3 +64,60 @@ for script_file in python_files:
 
 # processes_rest_server.wait()
 # print(f"Process {processes_rest_server.pid} finished")
+
+#After some time, kill a random storage node
+
+try:
+    N = int(sys.argv[1]) # Number of nodes to run
+
+    # List of Python files to run
+    python_files = ["storage-node.py" for _ in range(N)] + [ "rest-server.py"]
+
+    # Activate virtual environment
+    activate_env = r"C:\Users\m-esp\Documents\DistributedStorage\env\Scripts\activate.bat "  # Replace with the path to your virtualenv activation script
+
+
+    # Run each Python script with its corresponding arguments
+    processes_storage_nodes = []
+    processes_rest_server = None
+    i = 1
+    for script_file in python_files:
+        if i <= N:
+            print("Running ", script_file, "with args ",f"node0{i} ",f"{i}")
+            process = subprocess.Popen([activate_env, "&&","python", script_file] + [f"node{i}", f"{i}"])
+            print("Process ID: ", process.pid)
+            processes_storage_nodes.append(process)
+            i += 1
+        else:
+            print("Running ", script_file)
+            process = subprocess.Popen([activate_env, "&&","python", script_file] + [f" {N}"])
+            print("Process ID: ", process.pid)
+            processes_rest_server = process
+        sleep(0.2)
+
+
+    #After some time, kill a random storage node
+    sleep(5)
+    random_node = random.sample(range(N), 2)
+    print("Killing nodes ", [ id + 1 for id in random_node])
+    for id in random_node:
+        processes_storage_nodes[id].kill()
+
+
+#on keyboard interrupt, kill all processes
+    while True:
+        sleep(1)
+except KeyboardInterrupt:
+    print("\nKeyboard interrupt")
+    for process in processes_storage_nodes:
+        process.kill()
+    if processes_rest_server is not None:
+        processes_rest_server.kill()
+    print("All processes killed")
+except Exception as e:
+    print(e)
+    for process in processes_storage_nodes:
+        process.kill()
+    if processes_rest_server is not None:
+        processes_rest_server.kill()
+    print("All processes killed")
